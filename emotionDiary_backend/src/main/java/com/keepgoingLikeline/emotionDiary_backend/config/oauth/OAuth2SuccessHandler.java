6b20982d9e1aab90ca35a88bb698d8df229a7fe0
GamJaDo <kim0615157@gmail.com>
2024-01-31 09:50:57 +0900

    no message

---
+++ emotionDiary_backend/src/main/java/com/keepgoingLikeline/emotionDiary_backend/config/oauth/OAuth2SuccessHandler.java
@@ -0,0 +1,107 @@
+package com.keepgoingLikeline.emotionDiary_backend.config.oauth;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.keepgoingLikeline.emotionDiary_backend.config.jwt.TokenProvider;
+import com.keepgoingLikeline.emotionDiary_backend.entity.RefreshToken;
+import com.keepgoingLikeline.emotionDiary_backend.entity.UserEntity;
+import com.keepgoingLikeline.emotionDiary_backend.repository.RefreshTokenRepository;
+import com.keepgoingLikeline.emotionDiary_backend.service.UserService;
+import com.keepgoingLikeline.emotionDiary_backend.util.CookieUtil;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
+    public static final Duration REFRESH_TOKEN_DURATION = Duration.ofDays(14);
+    public static final Duration ACCESS_TOKEN_DURATION = Duration.ofDays(1);
+    public static final String REDIRECT_PATH = "/articles";
+
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final OAuth2AuthorizationRequestBasedOnCookieRepository authorizationRequestRepository;
+    private final UserService userService;
+    /*
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        UserEntity user = userService.findByEmail((String) oAuth2User.getAttributes().get("email"));
+
+        String refreshToken = tokenProvider.generateToken(user, REFRESH_TOKEN_DURATION);
+        saveRefreshToken(user.getId(), refreshToken);
+        addRefreshTokenToCookie(request, response, refreshToken);
+
+        String accessToken = tokenProvider.generateToken(user, ACCESS_TOKEN_DURATION);
+        String targetUrl = getTargetUrl(accessToken);
+
+        clearAuthenticationAttributes(request, response);
+
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+    */
+    
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        UserEntity user = userService.findByEmail((String) oAuth2User.getAttributes().get("email"));
+
+        String refreshToken = tokenProvider.generateToken(user, REFRESH_TOKEN_DURATION);
+        saveRefreshToken(user.getId(), refreshToken);
+        addRefreshTokenToCookie(request, response, refreshToken);
+
+        String accessToken = tokenProvider.generateToken(user, ACCESS_TOKEN_DURATION);
+
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("accessToken", accessToken);
+        tokens.put("refreshToken", refreshToken);
+
+        // ResponseEntity를 JSON으로 변환하여 응답 본문에 작성
+        response.setContentType("application/json;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.print(new ObjectMapper().writeValueAsString(tokens));
+        out.flush();
+    }
+
+    private void saveRefreshToken(Long userId, String newRefreshToken) {
+        RefreshToken refreshToken = refreshTokenRepository.findByUserId(userId)
+                .map(entity -> entity.update(newRefreshToken))
+                .orElse(new RefreshToken(userId, newRefreshToken));
+
+        refreshTokenRepository.save(refreshToken);
+    }
+    
+    private void addRefreshTokenToCookie(HttpServletRequest request, HttpServletResponse response, String refreshToken) {
+        int cookieMaxAge = (int) REFRESH_TOKEN_DURATION.toSeconds();
+
+        CookieUtil.deleteCookie(request, response, REFRESH_TOKEN_COOKIE_NAME);
+        CookieUtil.addCookie(response, REFRESH_TOKEN_COOKIE_NAME, refreshToken, cookieMaxAge);
+    }
+
+    private void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
+        super.clearAuthenticationAttributes(request);
+        authorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+    }
+
+    private String getTargetUrl(String token) {
+        return UriComponentsBuilder.fromUriString(REDIRECT_PATH)
+                .queryParam("token", token)
+                .build()
+                .toUriString();
+    }
+}
---
+++ emotionDiary_backend/src/main/java/com/keepgoingLikeline/emotionDiary_backend/service/RefreshTokenService.java
@@ -0,0 +1,22 @@
+package com.keepgoingLikeline.emotionDiary_backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.keepgoingLikeline.emotionDiary_backend.entity.RefreshToken;
+import com.keepgoingLikeline.emotionDiary_backend.repository.RefreshTokenRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+public class RefreshTokenService {
+	
+	@Autowired
+	private RefreshTokenRepository refreshTokenRepository;
+	
+	public RefreshToken findByRefreshToken(String refreshToken) {
+		
+		return refreshTokenRepository.findByRefreshToken(refreshToken)
+				.orElseThrow(() -> new IllegalArgumentException("Unexpecte token"));
+	}
+}
